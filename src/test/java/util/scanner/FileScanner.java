7b2cec05f63a6a2dc5275e511c2ac8024ea99924
Szymon Pirecki <szymon.pirecki@gmail.com>
2024-11-04 00:09:18 +0100

    Added file format parameter to merging method.

--- src/test/java/util/scanner/FileScanner.java
+++ src/test/java/util/scanner/FileScanner.java
@@ -1,5 +1,7 @@
 package util.scanner;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,14 +10,19 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
 
+@Slf4j
 public class FileScanner {
 
-    private static void mergeFeatureFiles() {
+    private static void mergeFiles(String fileFormat) {
         Path outputPath = Paths.get("src/test/resources/merged.txt");
 
         try (BufferedWriter writer = Files.newBufferedWriter(outputPath);
              Stream<Path> featureFiles = Files.walk(Paths.get("src/test/resources/features"))
-                     .filter(path -> path.toString().endsWith(".feature"))) {
+                     .filter(path -> path.toString().endsWith(fileFormat))) {
+
+            if (featureFiles.findAny().isEmpty()){
+                log.error("There are no {} files", fileFormat);
+            }
 
             for (Path path : (Iterable<Path>) featureFiles::iterator) {
                 List<String> lines = Files.readAllLines(path);
@@ -34,8 +41,6 @@ public class FileScanner {
     }
 
 
-    public static void main(String[] args) {
-        mergeFeatureFiles();
-    }
+
 }
 
